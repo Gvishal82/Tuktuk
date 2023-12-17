f58a42d1260e3e4dffd782e7354e08836c65d290
PheonixDeathNote <153576791+PheonixDeathNote@users.noreply.github.com>
2023-12-17 14:44:28 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) ACE 
+
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6775811275:AAEoE5BrrFIAdnhqs8B4LpMhDBBkKEhSOrk")
+    API_ID = int(os.environ["API_ID", 28590119]
+    API_HASH = os.environ["API_HASH", "2494557bf21e6c5152f26070aa1a97c7"]
+    AUTH_USERS = "6462391456"""
+
