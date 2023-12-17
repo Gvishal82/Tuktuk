a1feb9d8207c9b05f46e71aa76abfcef668cbf82
PheonixDeathNote <153576791+PheonixDeathNote@users.noreply.github.com>
2023-12-17 15:13:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,8 +8,8 @@ class Config(object):
     # get a token from @BotFather
     pass
     """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6775811275:AAEoE5BrrFIAdnhqs8B4LpMhDBBkKEhSOrk")
-    API_ID = int(os.environ["API_ID", 28590119]
-    API_HASH = os.environ["API_HASH", "2494557bf21e6c5152f26070aa1a97c7"]
-    AUTH_USERS = "6462391456"""
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6305751172:AAG3f0lLXE-ivB1r3xP0VSvUfR0nKfURYfM")
+    API_ID = int(os.environ["API_ID", 29759544]
+    API_HASH = os.environ["API_HASH", "97030a871720998c6efc5debcce67aa4"]
+    AUTH_USERS = "6952370497"""
 
